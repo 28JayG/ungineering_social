33991495d82866cd4a0c3819752b5237dc446b61
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2019-01-04 17:40:50 +0530

    Reg_page 0.1

---
+++ register_form.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Register Form</title>
+        <link rel="stylesheet" href="style.css"/>
+    </head>
+    <body>
+        <div class="container">
+            <div class="div1">
+                <div class="div11">
+                    <div class="updiv">
+                        <img src="Image/95146958-chat-speech-icon-e-commerce-sign-graph-symbol-for-your-web-site-design-logo-app-ui-vector-illustrati.jpg" alt="Status" class="image"/>
+                    </div>
+                </div>
+                <div class="div12">
+                    <p class="p">
+                        <b>New User<br/> Create <br/>Account<br/></b>
+                    </p>
+                    <a class="log1" href="http://127.0.0.1/social_media/login_form.php">Existing User Log-In</a>
+                </div>
+                <div class="lastdiv">   </div>
+            </div>
+            <div class="div2">
+                <div class="div21">
+                    
+                </div>
+                <div class="div221">
+                    <p1>
+                        <b>Create Account</b>
+                    </p1>
+                </div>
+                
+                <div class="div22">
+                    <div class="divx1">
+                        Name
+                    </div>
+                    <div class="divx2">
+                        <input type="text" name="name"/>
+                    </div>
+                </div>
+                <div class="div22">
+                    <div class="divx1">
+                        Email
+                    </div>
+                    <div class="divx2">
+                        <input type="text" name="email"/>
+                    </div>
+                </div>
+                <div class="div22">
+                    <div class="divx1">
+                        Password
+                    </div>
+                    <div class="divx2">
+                        <input type="password" id="pass" name="password"/>
+                    </div>
+                </div>
+                <div class="div22">
+                    <div class="divx1">
+                        Password
+                    </div>
+                    <div class="divx2">
+                        <input type="password" id="conpass" name="conf_pass"/>
+                    </div>
+                </div>
+                
+                <div class="div2222">
+                    <input style="color:white" type="submit" id="submit" class="sub" name="submit" value="Create Account"/>
+                </div>
+                <div class="div23">
+                    <a class="log" href="http://127.0.0.1/social_media/login_form.php">Existing User Log-In</a>
+                </div>
+                <div class="lastdiv">   </div>
+            </div>
+        </div>
+    </body>
+</html>
