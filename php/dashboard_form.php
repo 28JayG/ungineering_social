0fc1d202d22fc2bba8bc0bcc488a8653b04f078d
Ravi Shankar <ravishankar.rcc@gmail.com>
2019-01-06 18:58:51 +0530

    dashboard

---
+++ php/dashboard_form.php
@@ -0,0 +1,83 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/dashboard.css">
+    </head>
+    <body>
+        <div class="dashboard_wrapper">
+            <div class="header">
+                <div class="socail_media_logo">
+                    <img src="../images/logo.jpg" class="logo" alt="ungineering_logo">
+                    <p class="ungineering"><span style="color: red">un</span>gineering</p>
+                    <p class="bit">A <span style="color: red">bit</span> of knowledge is good.
+                    <p class="byte">A <span style="color: red">byte</span> is better.</p>
+                </div>
+                <div class="dashboard">
+                    My Dashboard
+                </div>
+                <div class="logout">
+                    <button id="logout">Logout</button>
+                </div>
+            </div>
+            <div class="account_details">
+                <form method="post" action="dashboard_form.php">
+                <div class="account_heading">
+                    <h1>My Account Details</h1>
+                </div>
+                <div class="name_detail">
+                    <div class="name">Name
+                    </div>    
+                    <div class="name_text">
+                        <input type="text" class="name1" name="name"/>
+                    </div>
+                </div>
+                <div class="email_detail">
+                    <div class="email">Email
+                    </div>
+                    <div class="email_text">
+                        <input type="text" class="email1" name="email"/>
+                    </div>
+                </div>
+                <div class="password_detail">
+                    <div class="password">Password
+                    </div>
+                    <div class="password_text">
+                        <input type="password" class="password1" name="password"/>
+                    </div>
+                </div>
+                <div class="college_detail">
+                    <div class="college">College
+                    </div>
+                    <div class="college_text">
+                        <input type="text" class="college1" name="college"/>
+                    </div>
+                </div>
+                <div class="phone_detail">
+                    <div class="phone">Phone Number
+                    </div>
+                    <div class="phone_text">
+                        <input type="text" class="phone1" name="phone_number"/>
+                    </div>
+                </div>
+                <div class="update">
+                    <input type="submit" name="update" class="update_dashboard" value="Update"/>
+                </div>
+                <form>
+            </div>
+            <div class="my_post">
+                <h1 id="post">My Post</h1>
+                <div class="status1">
+                    <p>Keep your face always toward the sunshine - and shadows will fall behind you.</p>
+                </div>
+                <div class="status2">
+                    <p>Do not pray for an easy life, pray for the strength to endure a difficult one.</p>
+                </div>
+                <div class="status3">
+                    <p>Write it on your heart that every day is the best day in the year.</p>
+                </div>
+            </div>
+            <div class="bottom_margin">
+            </div>
+        </div>
+    </body>
+</html>
+    
