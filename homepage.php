5f68014cfc01fe10e452be20c64ef0aead806ca0
Mugdha Agarwal <agarwalmugdha1998@gmail.com>
2019-01-07 15:29:01 +0530

    final homepage without loggedin done

--- homepage.php
+++ homepage.php
@@ -32,53 +32,47 @@
             $hostname = "localhost";
             $username = "root";
             $db_password = "123456";
-            $database = "ungineering";
+            $database = "social_media";
 
             $conn = mysqli_connect($hostname, $username, $db_password, $database);
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
 
-            $sql = "SELECT * FROM statuses";
-
+            //$sql = "SELECT * FROM statuses";
+            $sql =  "SELECT 
+                        statuses.status,statuses.date_time,users.name
+                    FROM
+                        statuses
+                    INNER JOIN 
+                        users
+                    ON
+                        statuses.user_id=users.id
+                    ORDER BY 
+                        statuses.date_time DESC";
+            
             $result = mysqli_query($conn, $sql);
             if (!$result) {
                 die("Error: " . $sql . "<br>" . mysqli_error($conn));
             }
             
             
-            while ($row=mysqli_fetch_array($result)) {
+            while ($row = mysqli_fetch_array($result)) {
             ?>
             <div class="b1">
                 <p>
                     <h1 class="h1"><?php echo $row['name']; ?></h1>
                      <p><?php echo $row['status']; ?></p>
-                    <p class="b11"><?php echo "Time:". echo $row['date_time'].echo "Hrs IST"?></p>
+                    <p class="b11"><?php echo $row['date_time']?></p>
                 
-            }
+            
                 </p>
             </div>    	
             <?php
-                mysqli_close($conn);
-        ?>
-
-        <!--<?php
-            for($i=0;$i<7;$i++){
-                ?>
-            <div class="b1">
-                <p>
-                    <h1 class="h1">Name</h1>
-                    <p>hello guys, this is my first status
-                        … At the same time, every minute, 10 hours of content were uploaded to the video sharing platform
-                        YouTube … According to Forrester Research, 75% of Internet surfers used “Social Media” in the
-                        quarter of 2008 by joining social networks, reading blogs, or contributing …</p>
-                    <p class="b11">Time:24:40 Hrs IST |26 Dec</p>
-                </p>
-            </div>
-            <?php
             }
-        ?>
-        </div>-->
+                mysqli_close($conn);
+        ?>        
+        </div>
     </body>
     //<script type="text/javascript" src="js/homepage.js"></script>
 </html> 
