79fb3bb926268479b63c5d0ceb0115d6d1c340f7
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2019-01-10 10:59:46 +0530

    Reg.+Login 0.7

--- register_form.php
+++ register_form.php
@@ -9,7 +9,7 @@
 <html>
     <head>
     <title>Register Form</title>
-        <link rel="stylesheet" href="test.css"/>
+        <link rel="stylesheet" href="css/style.css"/>
     </head>
     <body>
         <div class="container">
