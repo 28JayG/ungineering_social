cecdb52b0843c7ee0cf691f0aa73365166ec5b7b
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2019-01-10 10:36:17 +0530

    Reg.+Login 0.6

--- logout.php
+++ logout.php
@@ -1,8 +1,6 @@
 <?php
+    session_start();
     session_destroy();
-    session_unset();
+
     header('location: homepage.php');
-    // if(!isset($_SESSION['id'])){
-    //     header('location: homepage.php');
-    // }
 ?>
